1bd008c637d8d4b29c899ed03634242119119684
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-15 21:53:58 -0500

    nft

--- database/migrations/YYYY_MM_DD_HHMMSS_create_nfts_table.php
+++ database/migrations/YYYY_MM_DD_HHMMSS_create_nfts_table.php
@@ -1,1 +1,24 @@
- 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('nfts', function (Blueprint $table) {
+            $table->id();
+            $table->string('asset_id');
+            $table->string('name');
+            $table->string('owner');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('nfts');
+    }
+}; 
