e1e918d37a8032964b3149d2016affffefc438b7
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-18 01:03:31 -0500

    nuew

--- database/migrations/[timestamp]_create_nfts_table.php
+++ database/migrations/[timestamp]_create_nfts_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateNftsTable extends Migration
 {
     public function up()
     {
         Schema::create('nfts', function (Blueprint $table) {
             $table->id();
-            $table->string('asset_id');
-            $table->string('name');
-            $table->string('owner');
+            $table->foreignId('patient_id')->constrained();
+            $table->text('qr_code')->nullable();
             $table->timestamps();
         });
     }
@@ -21,4 +20,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('nfts');
     }
-}; 
+} 
