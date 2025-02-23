754ebf642b51a8d3b998ce92a689c213180b1eb8
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-23 15:02:55 -0500

    c

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,
-            //RoleSeeder::class,
-            UsersTableSeeder::class,
-            InitialDataSeeder::class,
-            InitialSeeder::class
+            InitialSeeder::class,
         ]);
     }
 }
