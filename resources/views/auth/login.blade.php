143dc24f3d9878fbb7db2e468552e026a315ee52
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-23 20:40:23 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,21 @@
 @extends('adminlte::auth.login')
 
-@section('title', 'Iniciar Sesión')
-
-@section('auth_header', 'Iniciar Sesión')
-
 @section('auth_body')
-    <form action="{{ route('login') }}" method="post">
+    <form method="POST" action="{{ route('login') }}" id="loginForm">
         @csrf
-
-        {{-- Email field --}}
+        
+        {{-- Email/Cedula field --}}
         <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                   value="{{ old('email') }}" placeholder="Email" autofocus>
+            <input type="text" name="login" class="form-control @error('login') is-invalid @enderror"
+                   value="{{ old('login') }}" placeholder="Email o Cédula" autofocus>
+
             <div class="input-group-append">
                 <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
                 </div>
             </div>
-            @error('email')
+
+            @error('login')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
@@ -28,11 +26,13 @@
         <div class="input-group mb-3">
             <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
                    placeholder="Contraseña">
+
             <div class="input-group-append">
                 <div class="input-group-text">
                     <span class="fas fa-lock"></span>
                 </div>
             </div>
+
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -43,14 +43,17 @@
         {{-- Login field --}}
         <div class="row">
             <div class="col-7">
-                <div class="icheck-primary">
-                    <input type="checkbox" name="remember" id="remember">
-                    <label for="remember">Recordarme</label>
+                <div class="icheck-primary" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
+                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label for="remember">
+                        {{ __('adminlte::adminlte.remember_me') }}
+                    </label>
                 </div>
             </div>
             <div class="col-5">
-                <button type="submit" class="btn btn-primary btn-block">
-                    Iniciar Sesión
+                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                    <span class="fas fa-sign-in-alt"></span>
+                    {{ __('adminlte::adminlte.sign_in') }}
                 </button>
             </div>
         </div>
@@ -58,21 +61,5 @@
 @stop
 
 @section('auth_footer')
-    @if (Route::has('password.request'))
-        <p class="my-0">
-            <a href="{{ route('password.request') }}">
-                ¿Olvidaste tu contraseña?
-            </a>
-        </p>
-    @endif
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script>
-        console.log('Login page loaded');
-    </script>
+    
 @stop
