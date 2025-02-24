a6e37ea1dabb158c814002ced22d134d0c05befa
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-23 19:52:44 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,9 +61,5 @@
 @stop
 
 @section('auth_footer')
-    <p class="my-0">
-        <a href="{{ route('password.request') }}">
-            {{ __('Olvidé mi contraseña') }}
-        </a>
-    </p>
+    
 @stop
