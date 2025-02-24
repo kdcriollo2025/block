6366a9aead5efdae431b5a925bac3b004b0ea97c
kdcriollo2025 <kdcriollo@espe.edu.ec>
2025-02-24 01:21:40 -0500

    Update 2024_08_27_070549_create_medicos_table.php

--- database/migrations/2024_08_27_070549_create_medicos_table.php
+++ database/migrations/2024_08_27_070549_create_medicos_table.php
@@ -15,17 +15,12 @@ return new class extends Migration
         
         Schema::create('medicos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('specialty')->nullable();
             $table->string('phone')->nullable();
             $table->string('cedula')->nullable();
             $table->boolean('estado')->default(true);
             $table->timestamps();
-
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
         });
     }
 
